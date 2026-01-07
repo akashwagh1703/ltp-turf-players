60ee24e5609b030549d68785401ab01060424a17
Akash Wagh <Akash.Wagh@esds.co.in>
2026-01-07 17:56:28 +0530

    Get master

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://35.222.74.225/api/v1/player',
+  baseURL: 'https://cidcomitra.com/ltp-apis/public/api/v1/player',
   headers: {
     'Content-Type': 'application/json',
   },
